370b66a478c5e1c13b1f56d0470a814dc7520f99
NalessoAxel <axelnalesso@gmail.com>
2024-05-06 17:12:45 +0200

    refactor(backend): Refactor API route declaration in app.ts

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,9 @@ import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-const apiRoutes = app.basePath('api').route("/expenses", expensesRoutes); // need to make this as variable so i can expose the type to my client
+// need to make this as variable so i can expose the type to my client
+
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoutes)
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 
