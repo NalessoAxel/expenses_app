c23145cdd44bb201a6791175bccdad8914ffde81
NalessoAxel <axelnalesso@gmail.com>
2024-05-17 09:57:06 +0200

    Feat: add more shared type

--- server/sharedTypes.ts
+++ server/sharedTypes.ts
@@ -1,6 +1,9 @@
+import type { z } from "zod";
 import { insertExpensesSchema } from "./db/schema/expense";
 
 export const createExpensesSchema = insertExpensesSchema.omit({
   userId: true,
   createdAt: true,
 });
+
+export type CreateExpense = z.infer<typeof createExpensesSchema>;
